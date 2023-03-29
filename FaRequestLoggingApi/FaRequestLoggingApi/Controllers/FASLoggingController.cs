da234e7facbc97034845b32f44f2c39d940cbdf2
dale-singh-absa <117896915+dale-singh-absa@users.noreply.github.com>
2023-03-29 19:10:13 +0200

    initial commit

---
+++ FaRequestLoggingApi/FaRequestLoggingApi/Controllers/FASLoggingController.cs
@@ -0,0 +1,71 @@
+﻿using FaRequestLoggingApi.Models;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace FaRequestLoggingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FASLoggingController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> PostAsync([FromBody] object body)
+        {
+            //await SaveS3(body);
+            await SaveDynamoDb(body);
+            return Ok("Logged");
+        }
+
+        private async Task SaveS3(object body)
+        {
+            var bucketName = "fa-siza-textract-src";
+            var objectName = Guid.NewGuid().ToString() + ".json";
+            using (var client = new AmazonS3Client(Amazon.RegionEndpoint.AFSouth1))
+            {
+                var request = new PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = objectName,
+                    ContentBody = body.ToString()
+                };
+                var response = await client.PutObjectAsync(request);
+            }
+        }
+
+        private async Task SaveDynamoDb(object body)
+        {
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.RegionEndpoint = RegionEndpoint.AFSouth1;
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient(clientConfig);
+            if (body != null)
+            {
+                var fasRequestLog = JsonConvert.DeserializeObject<RequestLog>(body.ToString() ?? "");
+                if (fasRequestLog != null)
+                {
+                    Table table = Table.LoadTable(client, "fas-logs");
+
+                    var log = new Document();
+                    log["TransactionId"] = fasRequestLog.TransactionId.ToString();
+                    log["LogTime"] = fasRequestLog.LogTime.ToString();
+                    log["TemplateId"] = fasRequestLog.TemplateId.ToString();
+                    log["ServiceName"] = fasRequestLog.ServiceName;
+                    log["MachineName"] = fasRequestLog.MachineName;
+                    log["LogType"] = fasRequestLog.LogType;
+                    log["ServiceRequestStatus"] = fasRequestLog.ServiceRequestStatus;
+                    log["RequestContent"] = fasRequestLog.RequestContent;
+                    log["ResponseContent"] = fasRequestLog.ResponseContent;
+                    log["ExceptionContent"] = fasRequestLog.ExceptionContent;
+
+                    await table.PutItemAsync(log);
+                }
+            }
+        }
+    }
+}
---
+++ FaRequestLoggingApi/FaRequestLoggingApi/Models/RequestLog.cs
@@ -0,0 +1,17 @@
+﻿namespace FaRequestLoggingApi.Models
+{
+    public class RequestLog
+    {
+        public string TransactionId { get; set; }
+        public DateTime LogTime { get; set; }
+        public int TemplateId { get; set; }
+        public string Environment { get; set; }
+        public string ServiceName { get; set; }
+        public string MachineName { get; set; }
+        public string LogType { get; set; }
+        public string ServiceRequestStatus { get; set; }
+        public string RequestContent { get; set; }
+        public string ResponseContent { get; set; }
+        public string ExceptionContent { get; set; }
+    }
+}
